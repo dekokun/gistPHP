a754b5c5339633f4aa844d5f5ca525a2e2b4accf
dekokun <shintaro.kurachi@gmail.com>
2013-01-02 17:22:31 +0900

    Slimをなんとなく動かしてみました。

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,4 +2,14 @@
 
 require_once dirname(__FILE__) . '/../lib/Autoload.php';
 
-system();
+$app = new Slim();
+
+$app->get('/', function () {
+    echo 'hoge';
+});
+
+$app->notFound(function () use ($app) {
+    echo '<html><body><h1>ページが見つかりません</h1></body></html>';
+});
+
+$app->run();
