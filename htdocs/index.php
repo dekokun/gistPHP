a99b38cefe8d29d181adf76583dea0d8d13e1a02
dekokun <shintaro.kurachi@gmail.com>
2013-01-09 10:45:24 +0900

    Repositoryの使用

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
+use TQ\Git\Cli\Binary;
+use TQ\Git\Repository\Repository;
 
+define('APP_DIR', dirname(__FILE__) . '/../');
+define('REPO_DIR', APP_DIR . 'repos/');
 
 $app = new Slim(array(
 ));
@@ -10,6 +14,22 @@ $app->view(new View());
 
 $app->get('/', function () {
     echo 'hoge';
+});
+
+$app->get('/repos/:repo_id/:commit_id', function ($repo_id, $commit_id) use($app) {
+    $repo_dir = REPO_DIR . $repo_id;
+    if (! file_exists($repo_dir)) {
+        $app->redirect('/404');
+    }
+    $git = Repository::open($repo_dir, new Binary('/usr/bin/env git'), 0755);
+    echo '<pre>';
+    var_dump($git->getCurrentBranch());
+    var_dump($git->getCurrentCommit());
+    var_dump($git->getStatus());
+    var_dump($git->getLog());
+    var_dump($git->showFile('hoge', $commit_id));
+    echo $repo_id;
+    echo '</pre>';
 });
 
 $app->notFound(function () use ($app) {
