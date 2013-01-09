88b241d09d2a4ef427814f6462fa8e6b3e67e571
dekokun <shintaro.kurachi@gmail.com>
2013-01-10 03:07:56 +0900

    ファイルが存在しない場合対応

--- htdocs/index.php
+++ htdocs/index.php
@@ -31,15 +31,25 @@ $app->get('/repos/:repo_id/edit', function ($repo_id) use($app, $binary) {
         $app->redirect('/404');
     }
     $in_repo_files = glob("$repo_dir/*");
-    foreach($in_repo_files as $file) {
-        $base_name = basename($file);
+    if(count($in_repo_files) > 0) {
+        foreach($in_repo_files as $file) {
+            $base_name = basename($file);
+            echo '<form method="POST" action="/repos/' . $repo_id . '">';
+            echo '<textarea name="' . $base_name . '">';
+            echo htmlspecialchars($git->showFile($base_name, 'HEAD'));
+            echo '</textarea>';
+            echo '<input type="hidden" name="_METHOD" value="PUT">';
+            echo '<input type="submit" value="Submit">';
+            echo '</form>';
+        }
+    } else {
         echo '<form method="POST" action="/repos/' . $repo_id . '">';
-        echo '<textarea name="' . $base_name . '">';
-        echo htmlspecialchars($git->showFile($base_name, 'HEAD'));
+        echo '<textarea name="index_txt">';
         echo '</textarea>';
         echo '<input type="hidden" name="_METHOD" value="PUT">';
         echo '<input type="submit" value="Submit">';
         echo '</form>';
+
     }
 
 });
