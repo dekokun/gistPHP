fee3989143c164f773956fad4dd76cfcfbdbcce6
dekokun <shintaro.kurachi@gmail.com>
2013-01-10 02:55:11 +0900

    更新ページとそれを受け取るページ作成

--- htdocs/index.php
+++ htdocs/index.php
@@ -21,6 +21,39 @@ $app->get('/repos/:repo_id', function ($repo_id) use($app) {
     $app->redirect("/repos/$repo_id/HEAD");
 });
 
+$app->get('/repos/:repo_id/edit', function ($repo_id) use($app, $binary) {
+    $repo_dir = REPO_DIR . $repo_id;
+    $git = Repository::open($repo_dir, $binary, 0755);
+    if (! file_exists($repo_dir)) {
+        $app->redirect('/404');
+    }
+    $in_repo_files = glob("$repo_dir/*");
+    foreach($in_repo_files as $file) {
+        $base_name = basename($file);
+        echo '<form method="POST" action="/repos/' . $repo_id . '">';
+        echo '<textarea name="' . $base_name . '">';
+        echo htmlspecialchars($git->showFile($base_name, 'HEAD'));
+        echo '</textarea>';
+        echo '<input type="hidden" name="_METHOD" value="PUT">';
+        echo '<input type="submit" value="Submit">';
+        echo '</form>';
+    }
+
+});
+
+$app->put('/repos/:repo_id', function ($repo_id) use($app, $binary) {
+    $repo_dir = REPO_DIR . $repo_id;
+    $git = Repository::open($repo_dir, $binary, 0755);
+    $post_vars = $app->request()->post();
+    foreach($post_vars as $key => $value) {
+        if ($key !== '_METHOD') {
+            $file_name = $key;
+            $git->writeFile($file_name, $value, date('Y/m/d H:i:s'));
+        }
+    }
+    $app->redirect("/repos/$repo_id/HEAD");
+});
+
 $app->get('/repos/:repo_id/:commit_id', function ($repo_id, $commit_id) use($app, $binary) {
     $repo_dir = REPO_DIR . $repo_id;
     if (! file_exists($repo_dir)) {
