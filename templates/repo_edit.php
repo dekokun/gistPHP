c2821bd4c51a610fde4212a8be91f11bfd65348f
dekokun <shintaro.kurachi@gmail.com>
2013-01-13 19:17:37 +0900

    textareaに不要なスペースが入る問題対応

--- templates/repo_edit.php
+++ templates/repo_edit.php
@@ -1,8 +1,6 @@
 <?php foreach($files_info as $file_info): ?>
 <form method="POST" action="/repos/<?php echo $repo_id ?>">
-  <textarea name="<?php echo $file_info['name'] ?>">
-    <?php echo $file_info['contents'] ?>
-  </textarea>
+  <textarea name="<?php echo $file_info['name'] ?>"><?php echo $file_info['contents'] ?></textarea>
   <input type="hidden" name="_METHOD" value="PUT">
   <input type="submit" value="Submit">
 </form>
