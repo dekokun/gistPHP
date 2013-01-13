88b24fcf7e51920f828585d8c222dc0a90aec9fd
dekokun <shintaro.kurachi@gmail.com>
2013-01-13 21:25:05 +0900

    fieldsetでinput typeを区切る

--- templates/repo_edit.php
+++ templates/repo_edit.php
@@ -1,10 +1,15 @@
 <form method="POST" action="/repos/<?php echo $repo_id ?>">
   <input type="hidden" name="_METHOD" value="PUT">
+  <fieldset>
+    <legend>既存ファイルの編集</legend>
 <?php foreach($files_info as $file_info): ?>
   <textarea name="<?php echo $file_info['name'] ?>"><?php echo $file_info['contents'] ?></textarea>
 <?php endforeach; ?>
-ファイル追加
+  </fieldset>
+  <fieldset>
+    <legend>ファイル追加</legend>
 <textarea name="index_txt_<?php echo $next_file_count?>">
 </textarea>
+  </fieldset>
 <input type="submit" value="Submit">
 </form>
