1019b225f3c09f57ca2fe70a503a409216b7c87d
dekokun <shintaro.kurachi@gmail.com>
2013-01-13 21:22:24 +0900

    hiddenタグはわかりやすいように上部に置いておく

--- templates/repo_edit.php
+++ templates/repo_edit.php
@@ -1,10 +1,10 @@
 <form method="POST" action="/repos/<?php echo $repo_id ?>">
+  <input type="hidden" name="_METHOD" value="PUT">
 <?php foreach($files_info as $file_info): ?>
   <textarea name="<?php echo $file_info['name'] ?>"><?php echo $file_info['contents'] ?></textarea>
 <?php endforeach; ?>
 ファイル追加
 <textarea name="index_txt_<?php echo $next_file_count?>">
 </textarea>
-<input type="hidden" name="_METHOD" value="PUT">
 <input type="submit" value="Submit">
 </form>
