36d626b1f5f165b2effad820fde654b5dc80035d
dekokun <shintaro.kurachi@gmail.com>
2013-01-13 21:21:10 +0900

    submitボタンを1つに

--- templates/repo_edit.php
+++ templates/repo_edit.php
@@ -1,8 +1,6 @@
 <form method="POST" action="/repos/<?php echo $repo_id ?>">
 <?php foreach($files_info as $file_info): ?>
   <textarea name="<?php echo $file_info['name'] ?>"><?php echo $file_info['contents'] ?></textarea>
-  <input type="hidden" name="_METHOD" value="PUT">
-  <input type="submit" value="Submit">
 <?php endforeach; ?>
 ファイル追加
 <textarea name="index_txt_<?php echo $next_file_count?>">
