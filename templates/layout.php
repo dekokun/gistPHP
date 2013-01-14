20c272a1eb089a191a438339d0a82cfa40ca4ba7
dekokun <shintaro.kurachi@gmail.com>
2013-01-14 12:26:49 +0900

    インデント修正

--- templates/layout.php
+++ templates/layout.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>test</title>
-  </head>
-  <body>
-    <?php echo $_html ?>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>test</title>
+</head>
+<body>
+<?php echo $_html ?>
+</body>
 </html>
