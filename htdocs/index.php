77f0d17310f05e8fba0bb7bc3e15a2870ee655e6
dekokun <shintaro.kurachi@gmail.com>
2013-01-14 23:18:05 +0900

    コメント部分削除

--- htdocs/index.php
+++ htdocs/index.php
@@ -80,20 +80,6 @@ $app->put('/repos/:repo_id', function ($repo_id) use ($app, $binary) {
 });
 
 $app->get('/repos/:repo_id/:commit_id', function ($repo_id, $commit_id) use ($app, $bookshelf) {
-//  $repo_dir = REPO_DIR . $repo_id;
-//  if (!file_exists($repo_dir)) {
-//    $app->redirect('/404');
-//  }
-//  $git = Repository::open($repo_dir, $binary, 0755);
-//  $in_repo_files = glob("$repo_dir/*");
-//  if (count($in_repo_files) === 0) {
-//    $app->redirect("/repos/$repo_id/edit");
-//  }
-//  $files = array();
-//  foreach ($in_repo_files as $file) {
-//    $base_name = basename($file);
-//    $files[] = $git->showFile($base_name, $commit_id);
-//  }
   $book = $bookshelf->showBook($repo_id, $commit_id);
   $app->render('repo.php',
     array(
