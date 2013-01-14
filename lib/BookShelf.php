8d530f9d210895f487a43adeb14c10f8d0ef5a2b
dekokun <shintaro.kurachi@gmail.com>
2013-01-14 21:22:07 +0900

    最大値は+1してからでよい

--- lib/BookShelf.php
+++ lib/BookShelf.php
@@ -21,7 +21,7 @@ class BookShelf
 
   public function makeNextBook()
   {
-    $now_max_book_id = max(array_map('basename', $this->books));
+    $now_max_book_id = max(array_map('basename', $this->books)) + 1;
     while (!@mkdir($book_place = $this->place . '/' . $now_max_book_id)) {
       $now_max_book_id += 1;
     }
