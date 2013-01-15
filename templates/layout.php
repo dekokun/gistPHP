d07ef6c58a7d35f549e9197eabe27563201a4e4f
dekokun <shintaro.kurachi@gmail.com>
2013-01-15 19:17:02 +0900

    わかりやすいように、code-prettifyのものは一箇所にまとめた

--- templates/layout.php
+++ templates/layout.php
@@ -3,13 +3,13 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo isset($title) ? $title . ' - '  : ''?>gistphp</title>
-  <link href="/css/prettify.css" type="text/css" rel="stylesheet"></link>
+  <link href="/code_prettify/prettify.css" type="text/css" rel="stylesheet"></link>
 </head>
 <body>
 <?php echo $_html ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <!-- Code syntax highlight -->
-<script type="text/javascript " src="/js/prettify.js"></script>
+<script type="text/javascript " src="/code_prettify/prettify.js"></script>
 <script type="text/javascript">
   $(function(){
     $('pre').css({
